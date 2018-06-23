623982f24381777e86f3799cc7adba6a15f0285b
Arvid Nilber <aanilber@gmail.com>
2018-06-24 01:41:11 +0200

    asd

---
+++ packages/web/src/modules/register/ui/RegisterView.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import Button from '@material-ui/core/Button';
+
+export class RegisterView extends React.PureComponent {
+	render() {
+		return <Button type="primary" variant="contained" color="primary">Button</Button>;
+		}
+}
