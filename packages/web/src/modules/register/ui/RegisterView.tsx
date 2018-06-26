80a1c03413229a7d83cbf75702365210265801fe
Arvid Nilber <aanilber@gmail.com>
2018-06-26 23:30:20 +0200

    Fixed some stuff

--- packages/web/src/modules/register/ui/RegisterView.tsx
+++ packages/web/src/modules/register/ui/RegisterView.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react'
 import { TextField, Button } from '@material-ui/core/';
 import "./Register.css";
+import { Form } from 'antd';
 import { withFormik, FormikErrors, FormikProps } from 'formik';
+import * as yup from 'yup';
+const FormItem = Form.Item;
 
 interface FormValues {
 	email: string;
@@ -13,48 +16,73 @@ interface Props {
 }
 
 class C extends React.PureComponent<FormikProps<FormValues> & Props> {
-		  render() {
-			const {values, handleChange, handleBlur, handleSubmit} = this.props;
-		return (
-		  <div className="intro">
-			<h1>Welcome to Airbnb</h1>
-			<h2>First, lets create an account!</h2>  
-			<form className="container" onSubmit={handleSubmit}>
-						<TextField
-						name="email"
-						id="email"
-						label="Email"
-						margin="normal"
-						className="textInput"
-						value={values.email}
-						onChange={handleChange}
-						onBlur={handleBlur}
-						/>
-						<TextField
-						name="password"
-						id="password"
-						label="Password"
-						margin="normal"
-						type="password"
-						value={values.password}
-						onChange={handleChange}
-						onBlur={handleBlur}
-						className="textInput"
-						/>
-					<Button type="primary" className="forgot" href="">Forgot password</Button>
-					<Button value="submit" variant="contained" type="primary" color="secondary" className="buttonForm">
-						Register
-					</Button>
-					<div className="login">
-						Or <a href="">login now!</a>
-					</div>
-				</form>
-			</div>
-		);
+		render() {
+		const {values, handleChange, handleBlur, handleSubmit, touched, errors} = this.props;
+		console.log(errors)	
+			return (
+				<div className="intro">
+				<h1>Welcome to Airbnb</h1>
+				<h2>First, lets create an account!</h2>  
+				<form className="container" onSubmit={handleSubmit}>
+						<FormItem help={touched.email && errors.email ? errors.email : ""}>
+							<TextField
+							name="email"
+							id="email"
+							label="Email"
+							className="textInput"
+							margin="normal"
+							value={values.email}
+							onChange={handleChange}
+							onBlur={handleBlur}
+							/>
+						</FormItem>
+						<FormItem help={touched.password && errors.password ? errors.password : ""}>
+							<TextField
+							name="password"
+							id="password"
+							label="Password"
+							type="password"
+							className="textInput"
+							value={values.password}
+							onChange={handleChange}
+							onBlur={handleBlur}
+							/>
+						</FormItem>
+						<Button className="forgot" href="">Forgot password</Button>
+						<Button value="submit" variant="contained" type="primary" color="secondary" className="buttonForm">
+							Register
+						</Button>
+						<div className="login">
+							Or <a href="">login now!</a>
+						</div>
+					</form>
+				</div>
+			);
 	  }
 	}
+const emailNotLongEnough = "email must be at least 3 characters"; 
+const passwordNotLongEnough = "password must be at least 3 characters";
+const invalidEmail = "email must be a valid email";
+
+const registerPasswordValidation = yup
+	.string()
+	.min(3, passwordNotLongEnough)
+	.max(255)
+	.required();
+
+const validationSchema = yup.object().shape({
+	email: yup
+		.string()
+		.min(3, emailNotLongEnough)
+		.max(255)
+		.email(invalidEmail)
+		.required(),
+	password: registerPasswordValidation
+});
+	
 
 export const RegisterView = withFormik<Props, FormValues>({
+	validationSchema,
 	mapPropsToValues: () => ({ email: "", password:""}),
 	handleSubmit: async (values, {props, setErrors}) => {
 		const errors = await props.submit(values);
