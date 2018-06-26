029cecb0fb4629aa6c301496d41d405a36c570c6
Arvid Nilber <aanilber@gmail.com>
2018-06-26 21:54:04 +0200

    Fixed the register page

--- packages/web/src/modules/register/RegisterConnector.tsx
+++ packages/web/src/modules/register/RegisterConnector.tsx
@@ -1,4 +1,17 @@
-import * as React from 'react';
-import { RegisterView } from './ui/RegisterView';
+import * as React from "react";
+import { RegisterView } from "./ui/RegisterView";
 
-export const RegisterConnector = () => <RegisterView/>;
+// container -> view
+// container -> connector -> view
+// controller -> connector -> view
+
+export class RegisterConnector extends React.PureComponent {
+  dummySubmit = async (values: any) => {
+    console.log(values);
+    return null;
+  };
+
+  render() {
+    return <RegisterView submit={this.dummySubmit} />;
+  }
+}
--- packages/web/src/modules/register/ui/RegisterView.tsx
+++ packages/web/src/modules/register/ui/RegisterView.tsx
@@ -1,8 +1,65 @@
 import * as React from 'react'
-import Button from '@material-ui/core/Button';
+import { TextField, Button } from '@material-ui/core/';
+import "./Register.css";
+import { withFormik, FormikErrors, FormikProps } from 'formik';
 
-export class RegisterView extends React.PureComponent {
-	render() {
-		return <Button type="primary" variant="contained" color="primary">Button</Button>;
-		}
+interface FormValues {
+	email: string;
+	password: string;
 }
+
+interface Props {
+  submit: (values: FormValues) => Promise<FormikErrors<FormValues> | null>;
+}
+
+class C extends React.PureComponent<FormikProps<FormValues> & Props> {
+		  render() {
+			const {values, handleChange, handleBlur, handleSubmit} = this.props;
+		return (
+		  <div className="intro">
+			<h1>Welcome to Airbnb</h1>
+			<h2>First, lets create an account!</h2>  
+			<form className="container" onSubmit={handleSubmit}>
+						<TextField
+						name="email"
+						id="email"
+						label="Email"
+						margin="normal"
+						className="textInput"
+						value={values.email}
+						onChange={handleChange}
+						onBlur={handleBlur}
+						/>
+						<TextField
+						name="password"
+						id="password"
+						label="Password"
+						margin="normal"
+						type="password"
+						value={values.password}
+						onChange={handleChange}
+						onBlur={handleBlur}
+						className="textInput"
+						/>
+					<Button type="primary" className="forgot" href="">Forgot password</Button>
+					<Button value="submit" variant="contained" type="primary" color="secondary" className="buttonForm">
+						Register
+					</Button>
+					<div className="login">
+						Or <a href="">login now!</a>
+					</div>
+				</form>
+			</div>
+		);
+	  }
+	}
+
+export const RegisterView = withFormik<Props, FormValues>({
+	mapPropsToValues: () => ({ email: "", password:""}),
+	handleSubmit: async (values, {props, setErrors}) => {
+		const errors = await props.submit(values);
+		if (errors) {
+			setErrors(errors);
+		}	
+	}
+})(C);
